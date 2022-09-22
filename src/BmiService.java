27fa730882ca97d64648cfbdb7cbedb4123e77b3
Dmitry Simdyanov <newtemnota108@gmail.com>
2022-09-22 23:03:02 +0400

    add reformat code

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,10 +1,10 @@
 public class BmiService {
     public double calculate(double weight, double height) {
         double result;
-        if (height> 0) {
-            result=weight/height/height;
+        if (height > 0) {
+            result = weight / height / height;
         } else {
-            result=0;
+            result = 0;
         }
         return result;
     }
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         BmiService service = new BmiService();
         double weight = 75.5; // введите Ваш вес в килограммах
         double height = 1.80; // введите Ваш рост в метрах
-        double bmi = service.calculate (weight, height);
+        double bmi = service.calculate(weight, height);
         System.out.println("Ваш индекс массы тела составляет: ");
         System.out.println(bmi);
     }
